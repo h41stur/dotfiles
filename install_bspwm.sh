1769269495abb42507c33783972f9f7f80edcb38
H41stur <leonardor.toledo+1@gmail.com>
2022-01-14 12:14:16 -0500

    Install options

--- install_bspwm.sh
+++ install_bspwm.sh
@@ -1,14 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+then
+		echo "Rode com sudo!"
+		exit
+fi
 
-echo "Qual SO?\n"
-echo "\n[1] Arch\n[2] Debian Based\n"
+echo -e "Qual SO?\n"
+echo -e "\n[1] Arch\n[2] Debian Based\n"
 
 read -p '> ' OS
 
 DEP=$(which bspwm sxhkd | wc -l)
+HOME=$(pwd)
+USERINSTALL=$SUDO_USER
 
-if [ $OS = '1' ]
+if [ $OS -eq 1 ]
 then
 		INSTALLER="pacman -S"
 else
@@ -18,26 +25,27 @@ fi
 # INSTALLER
 installer() {
 
-	# UPDATING
-	if [ $OS = '1'  ]
-	then
-			sudo pacman -Syu
-	else
-			sudo apt update && sudo apt upgrade -y
-	fi
+		# UPDATE
+		if [ $OS -eq 1 ]
+		then
+				pacman -Syu
+		else
+				apt update && apt upgrade -y
+		fi
 
-	# INSTALLING BSPWM
-	sudo $INSTALLER bspwm sxhkd
-	mkdir -p ~/.config/{bspwm,sxhkd}
-	cp bspwm/bspwmrc ~/.config/bspwm/
-	if [ $OS = '1'  ]
-	then
-			cp sxhkd/sxhkdrcarch ~/.config/sxhkd/sxhkdrc
-	else
-			cp sxhkd/sxhkdrcdeb ~/.config/sxhkd/sxhkdrc
-	fi
+		# INSTALLING BSPWM
+		$INSTALLER bspwm sxhkd
+		sudo -u $USERINSTALL mkdir -p /home/$USERINSTALL/.config/{bspwm,sxhkd}
+		sudo -u $USERINSTALL cp bspwm/bspwmrc /home/$USERINSTALL/.config/bspwm/
+		if [ $OS = '1' ]
+		then
+				sudo -u $USERINSTALL cp sxhkd/sxhkdrcarch /home/$USERINSTALL/.config/sxhkd/sxhkdrc
+		else
+				sudo -u $USERINSTALL cp sxhkd/sxhkdrcdeb /home/$USERINSTALL/.config/sxhkd/sxhkdrc
+		fi
 
-	chmod +x ~/.config/bspwm/bspwmrc ~/.config/sxhkd/sxhkdrc
+		sudo -u $USERINSTALL chmod +x /home/$USERINSTALL/.config/bspwm/bspwmrc /home/$USERINSTALL/.config/sxhkd/sxhkdrc
+		shutdown -r now
 }
 
 if [ $DEP -lt '2' ]
@@ -46,31 +54,46 @@ then
 fi
 
 # FEH ROFI COMPTON
-sudo $INSTALLER feh rofi compton
-cp -r .wallpapers/ ~/
-cp .fehbg ~/
-cp -r rofi/ ~/.config/
+$INSTALLER feh rofi compton
+sudo -u $USERINSTALL cp -r .wallpapers/ /home/$USERINSTALL
+sudo -u $USERINSTALL cp .fehbg /home/$USERINSTALL
+sudo -u $USERINSTALL cp -r rofi/ /home/$USERINSTALL/.config/
 
 # NUMLOCKX
-sudo $INSTALLER numlockx
+$INSTALLER numlockx
 
 # FONTS
-mkdir -p ~/.local/fonts
-cp fonts/*.ttf ~/.local/fonts/
-cp fonts/*.otf ~/.local/fonts/
-fc-cache -fv
+sudo -u $USERINSTALL mkdir -p /home/$USERINSTALL/.local/share/fonts
+sudo -u $USERINSTALL cp -r fonts/* /home/$USERINSTALL/.local/share/fonts/
+sudo -u $USERINSTALL fc-cache -fv
 
 # POLYBAR
-sudo $INSTALLER polybar
-cp -r polybar/ ~/.config/
+if [ $OS = '1' ]
+then
+	YAY=$(which yay | wc -l)
+	if [ $YAY -lt '1' ]
+	then
+		pacman -S --needed base-devel
+		cd ~
+		sudo -u $USERINSTALL git clone https://aur.archlinux.org/yay.git
+		cd yay/
+		sudo -u $USERINSTALL makepkg -si
+		cd $HOME
+	fi
+	sudo -u $USERINSTALL yay -S polybar
+else
+	apt install polybar
+fi
+	
+sudo -u $USERINSTALL cp -r polybar/ /home/$USERINSTALL/.config/
 
 # CASE INSENSITIVE
-cp .inputrc ~/
+sudo -u $USERINSTALL cp .inputrc /home/$USERINSTALL/
 
 # KEYBOARD
-cp -r .prog/ ~/
-sudo ln -sf $HOME/.prog/keyb /usr/bin/keyb
-sudo cp -f keyboard /etc/default/
+sudo -u $USERINSTALL cp -r .prog/ /home/$USERINSTALL/
+ln -sf $HOME/.prog/keyb /usr/bin/keyb
+cp -f keyboard /etc/default/
 
 # RESTART
-sudo shutdown -r now
+shutdown -r now
